4924eae3e0539560df7f2b6752360b0163f32c4a
mijung-oh <ks03102@naver.com>
2021-12-30 23:33:58 +0900

    feat: 계급데이터 함수 최종

--- API문서.md
+++ API문서.md
@@ -1,21 +1,21 @@
 # 📃 API
 
-| Description          | Function                                | parameter                                                    | return                                                       | 비고           |
-| -------------------- | --------------------------------------- | ------------------------------------------------------------ | ------------------------------------------------------------ | -------------- |
-| 전체 데이터 가져오기 | getTotalData()                          |                                                              | {키워드: {<br />"sentences": [<br />idx: {<br />"sentence": string,<br />"group": string,<br />"count": string,<br />"id": integer<br />}]<br />"totalCount": string<br />}<br />} |                |
-| 데이터 저장          | setData()                               |                                                              |                                                              |                |
-| 그룹 데이터 저장하기 | addGroups (group)                       | group: 그룹이름(string)                                      |                                                              |                |
-| 그룹 데이터 가져오기 | getGroups()                             |                                                              | [<br />idx: 그룹이름<br />]                                  | 백에서만 사용  |
-| 카운트 수정          | updateCount(group, keyword, sentenceId) | group: 그룹이름(string),<br />keyword: 키워드(string),<br />sentenceId: 문장 id(integer) |                                                              | count 1씩 증가 |
-| 키워드 삭제          | deleteKeyword(group, keyword)           | group: 그룹이름(string)<br />keyword: 키워드(string)         |                                                              |                |
-| 비밀번호 저장        | setPassword(password)                   | password: 비밀번호(string)                                   |                                                              |                |
-| 비밀번호 수정        | updatePassword(newPassword)             | newPassword: 비밀번호(string)                                |                                                              |                |
-| 계급데이터 가져오기  | getDataByGroups()                       |                                                              |                                                              |                |
-|                      |                                         |                                                              |                                                              |                |
-|                      |                                         |                                                              |                                                              |                |
-|                      |                                         |                                                              |                                                              |                |
-|                      |                                         |                                                              |                                                              |                |
-|                      |                                         |                                                              |                                                              |                |
+| Description              | Function                                | parameter                                                    | return                                                       | 비고           |
+| ------------------------ | --------------------------------------- | ------------------------------------------------------------ | ------------------------------------------------------------ | -------------- |
+| **전체 데이터 가져오기** | getTotalData()                          |                                                              | {키워드: {<br />"sentences": [<br />idx: {<br />"sentence": string,<br />"group": string,<br />"count": string,<br />"id": integer<br />}]<br />"totalCount": string<br />}<br />} |                |
+| **데이터 저장**          | setData()                               |                                                              |                                                              |                |
+| **그룹 데이터 저장하기** | addGroups (group)                       | group: 그룹이름(string)                                      |                                                              |                |
+| **그룹 데이터 가져오기** | getGroups()                             |                                                              | [<br />idx: 그룹이름<br />]                                  | 백에서만 사용  |
+| **카운트 수정**          | updateCount(group, keyword, sentenceId) | group: 그룹이름(string),<br />keyword: 키워드(string),<br />sentenceId: 문장 id(integer) |                                                              | count 1씩 증가 |
+| **키워드 삭제**          | deleteKeyword(group, keyword)           | group: 그룹이름(string)<br />keyword: 키워드(string)         |                                                              |                |
+| **비밀번호 저장**        | setPassword(password)                   | password: 비밀번호(string)                                   |                                                              |                |
+| **비밀번호 수정**        | updatePassword(newPassword)             | newPassword: 비밀번호(string)                                |                                                              |                |
+| **계급데이터 가져오기**  | getDataByGroups()                       |                                                              | {<br />keyword: {<br />group1: 60,<br />group2: 40,<br />group3: 10,<br />},<br />keyword2: {}} |                |
+|                          |                                         |                                                              |                                                              |                |
+|                          |                                         |                                                              |                                                              |                |
+|                          |                                         |                                                              |                                                              |                |
+|                          |                                         |                                                              |                                                              |                |
+|                          |                                         |                                                              |                                                              |                |
 
 
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
-import { getDocs, collection } from "firebase/firestore"
+// import { getDocs, collection } from "firebase/firestore"
 import { doc } from "firebase/firestore"
-import { setDoc } from "firebase/firestore"
-import { deleteDoc } from "firebase/firestore"
-// import { collection,  addDoc } from "firebase/firestore";
-// import {  updateDoc, getDoc } from "firebase/firestore";
+// import { setDoc } from "firebase/firestore"
+// import { deleteDoc } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore";
+import {  updateDoc, getDoc, increment } from "firebase/firestore";
 initializeApp({
   apiKey: "AIzaSyCKZQz6LPTVvBzbG4gWiFW_mv_A-Gvqo7k",
   authDomain: "bubble-chart-89dd0.firebaseapp.com",
@@ -39,7 +39,8 @@ async function getTotalData() {
 
       let sentences = docsInCollection[i].doc.data.value.mapValue.fields
       
-      result[keyword]["totalCount"] = sentences.totalCount.integerValue
+      // result[keyword]["totalCount"] = sentences.totalCount.integerValue
+      result[keyword]["totalCount"] = await getTotalCountByKeyword(keyword)
       
       sentences = Object.entries(sentences)
 
@@ -57,13 +58,9 @@ async function getTotalData() {
       }
     }
   }
+  console.log("버블데이터", result)
   return result
 }
-
-// 누적 totalCount
-// function setTotalCount() {
-  
-// }
 
 // export default {
 //   getTotalData: async function() {
@@ -106,53 +103,52 @@ async function getTotalData() {
 //     return result
 //   }
 // }
-// 비밀번호 저장/가져오는 api
 
 // 데이터 추가
-async function setData(dataCollection) {
-  let groups = []
-  // 기존 데이터 삭제
-  for (let i in dataCollection) {
-    groups.push(dataCollection[i]["group"])
-  }
-  groups = new Set(groups)
-  groups.forEach((g) => {
-    deleteDocs(g)
-  })
+// async function setData(dataCollection) {
+//   let groups = []
+//   // 기존 데이터 삭제
+//   for (let i in dataCollection) {
+//     groups.push(dataCollection[i]["group"])
+//   }
+//   groups = new Set(groups)
+//   groups.forEach((g) => {
+//     deleteDocs(g)
+//   })
 
-  // 그룹별로 저장
-  for (let i in dataCollection) {
+//   // 그룹별로 저장
+//   for (let i in dataCollection) {
     
-    let data = new Object()
-    let keyword = dataCollection[i]["keyword"]
-    let group = dataCollection[i]["group"]
+//     let data = new Object()
+//     let keyword = dataCollection[i]["keyword"]
+//     let group = dataCollection[i]["group"]
 
-    data["totalCount"] = dataCollection[i]["totalCount"]
+//     data["totalCount"] = dataCollection[i]["totalCount"]
 
-    for (let j in dataCollection[i]["sentences"]) {
-      data[j] = {
-        "sentence": dataCollection[i]["sentences"][j],
-        "count": 0,
-        "group": group
-      }
-    }
-    await setDoc(doc(db, group, keyword), data)
-  }
+//     for (let j in dataCollection[i]["sentences"]) {
+//       data[j] = {
+//         "sentence": dataCollection[i]["sentences"][j],
+//         "count": 0,
+//         "group": group
+//       }
+//     }
+//     await setDoc(doc(db, group, keyword), data)
+//   }
 
-  // 키워드별로 저장
-  for (let i in dataCollection) {
+//   // 키워드별로 저장
+//   for (let i in dataCollection) {
     
-    let data = new Object()
-    let keyword = dataCollection[i]["keyword"]
-    let group = dataCollection[i]["group"]
+//     let data = new Object()
+//     let keyword = dataCollection[i]["keyword"]
+//     let group = dataCollection[i]["group"]
 
-    data["totalCount"] = dataCollection[i]["totalCount"]
-    data["sentences"] = dataCollection[i]["sentences"]
+//     data["totalCount"] = dataCollection[i]["totalCount"]
+//     data["sentences"] = dataCollection[i]["sentences"]
 
-    await setDoc(doc(db, keyword, group), data)
-  }
+//     await setDoc(doc(db, keyword, group), data)
+//   }
   
-}
+// }
 
 // 키워드별 토탈카운트
 async function getTotalCountByKeyword(keyword) {
@@ -166,80 +162,85 @@ async function getTotalCountByKeyword(keyword) {
 getTotalCountByKeyword("keyword1")
 
 // 키워드별 문장들
-async function sentencesByKeyword(keyword) {
-  let result = {}
-  const collections = await getDocs(collection(db, keyword))
-  collections.forEach((document) => {
-    let path = document._key.path.segments
-    let group = path[path.length-1]
+// async function sentencesByKeyword(keyword) {
+//   let result = {}
+//   const collections = await getDocs(collection(db, keyword))
+//   collections.forEach((document) => {
+//     let path = document._key.path.segments
+//     let group = path[path.length-1]
 
-    let sentences = document.data().sentences
-    result[group] = sentences
-  })
-  return result
-}
-sentencesByKeyword("keyword1")
+//     let sentences = document.data().sentences
+//     result[group] = sentences
+//   })
+//   return result
+// }
+// sentencesByKeyword("keyword1")
 
 // 계급데이터 가져오기
-async function getDataByGroups() {
-  let groupNameList = await getGroups()
-  let result = {}
+// async function getDataByGroups() {
+//   let groupNameList = await getGroups()
+//   let result = {}
 
-  // 키워드당 빈 배열을 만들어주기
-  for (let i in groupNameList) {
-    const collections = await getDocs(collection(db, groupNameList[i]))
-    let docsInCollection = collections._snapshot.docChanges
+//   // 키워드당 빈 배열을 만들어주기
+//   for (let i in groupNameList) {
+//     const collections = await getDocs(collection(db, groupNameList[i]))
+//     let docsInCollection = collections._snapshot.docChanges
     
-    for(let i=0; i< docsInCollection.length; i++) {
-      let path = docsInCollection[i].doc.key.path.segments
-      let keyword = path[path.length-1]
-      result[keyword] = {}
-    }
-  }
+//     for(let i=0; i< docsInCollection.length; i++) {
+//       let path = docsInCollection[i].doc.key.path.segments
+//       let keyword = path[path.length-1]
+//       result[keyword] = {}
+//     }
+//   }
 
-  // 그룹 순회하며 하나씩 가져오기
-  for (let i in groupNameList) {
-    const collections = await getDocs(collection(db, groupNameList[i]))
-    let group = groupNameList[i]
-    let docsInCollection = collections._snapshot.docChanges
+//   // 그룹 순회하며 하나씩 가져오기
+//   for (let i in groupNameList) {
+//     const collections = await getDocs(collection(db, groupNameList[i]))
+//     let group = groupNameList[i]
+//     let docsInCollection = collections._snapshot.docChanges
     
-    // 해당 그룹안에 있는 docs(키워드) 순회
-    for(let i=0; i< docsInCollection.length; i++) {
-      let path = docsInCollection[i].doc.key.path.segments
-      let keyword = path[path.length-1]
+//     // 해당 그룹안에 있는 docs(키워드) 순회
+//     for(let i=0; i< docsInCollection.length; i++) {
+//       let path = docsInCollection[i].doc.key.path.segments
+//       let keyword = path[path.length-1]
 
-      // let pushData = {}
-      let value = await getTotalCountByKeyword(keyword)
-      result[keyword][group] = value
-      // pushData[group] = value
-      // console.log(pushData)
-      // result[keyword].push(pushData)
-    }
-  }
-  console.log("계급데이터", result)
-  return result
-}
-getDataByGroups()
+//       // let pushData = {}
+//       let value = await getTotalCountByKeyword(keyword)
+//       result[keyword][group] = value
+//       // pushData[group] = value
+//       // console.log(pushData)
+//       // result[keyword].push(pushData)
+//     }
+//   }
+//   console.log("계급데이터", result)
+//   return result
+// }
+// getDataByGroups()
 
 // 그룹 데이터 저장
-function addGroups (groups) {
-  for (let group in groups) {
-    setDoc(doc(db, "Groups", groups[group]), {"groupName": groups[group]})
-  }
-}
+// function addGroups (groups) {
+//   for (let group in groups) {
+//     setDoc(doc(db, "Groups", groups[group]), {"groupName": groups[group]})
+//   }
+// }
 
 // 비밀번호 저장
-function setPassword (password) {
-  setDoc(doc(db, "Password", "password"), {"password": password})
-}
-setPassword("admin")
+// function setPassword (password) {
+//   setDoc(doc(db, "Password", "password"), {"password": password})
+// }
+// setPassword("admin")
+
+// async function getPassword () {
+//   let passwordDoc = await getDoc(doc(db, "Password", "password"))
+//   console.log(passwordDoc)
+// }
 
 // 비밀번호 수정
-function updatePassword(newPassword) {
-  deleteDoc(doc(db, "Password", "password"));
-  setDoc(doc(db, "Password", "password"), {"password": newPassword})
-}
-updatePassword("admin2")
+// function updatePassword(newPassword) {
+//   deleteDoc(doc(db, "Password", "password"));
+//   setDoc(doc(db, "Password", "password"), {"password": newPassword})
+// }
+// updatePassword("admin2")
 
 // 그룹 가져오는 api
 async function getGroups() {
@@ -257,81 +258,73 @@ async function getGroups() {
 }
 
 // 카운트 수정
-// function updateCount(group, keyword, sentenceId, countNumber) {
-  
-//   const findDoc = getDoc(doc(db, group, keyword))
-//   let changeContent = {}
-  
-//   findDoc.then((res) => {
-//     let docInCollection = res._document.data.value.mapValue.fields
-//     let fieldToModify = docInCollection[sentenceId].mapValue.fields
+async function updateCount(group, keyword, sentenceId) {
+  const findDoc = await getDoc(doc(db, group, keyword))
+  let changeContent = {}
+
+  let fieldToModify = findDoc.data()[sentenceId]
     
-//     let cur_group = fieldToModify["group"].stringValue 
-//     let cur_sentence = fieldToModify["sentence"].stringValue 
-//     // let cur_count = (parseInt(fieldToModify["count"].integerValue) + 1)
+    let cur_group = fieldToModify["group"]
+    let cur_sentence = fieldToModify["sentence"]
+    let cur_count = (parseInt(fieldToModify["count"]) + 1)
     
-//     changeContent[sentenceId] = {
-//       "count": increment(countNumber),
-//       "group": cur_group,
-//       "sentence": cur_sentence
-//     }
+    changeContent[sentenceId] = {
+      "count": increment(cur_count),
+      "group": cur_group,
+      "sentence": cur_sentence
+    }
 
-//     updateDoc(doc(db, group, keyword), changeContent)
-//   })
+    updateDoc(doc(db, group, keyword), changeContent)
   
-//   console.log("changeContent: ", changeContent)
+  console.log("changeContent: ", changeContent)
 
-// }
-
+}
+updateCount("group1", "keyword1", 0) 
 // 키워드 삭제 api
 // function deleteKeyword(group, keyword) {
 //   deleteDoc(doc(db, group, keyword));
 // }
 
 // docs 데이터 삭제
-async function deleteDocs(collectionName) {
-  console.log("in deleteDocs", collectionName)
-  const collections = await getDocs(collection(db, collectionName))
+// async function deleteDocs(collectionName) {
+//   const collections = await getDocs(collection(db, collectionName))
 
-  collections.forEach((document) => {
-    console.log(document.data().group)
-    deleteDoc(doc(db, collectionName, document.data().groupName));
-  })
-}
+//   collections.forEach((document) => {
+//     console.log(document.data().group)
+//     deleteDoc(doc(db, collectionName, document.data().groupName));
+//   })
+// }
 
 
 
 getTotalData()
-// 함수 실행
-// setData(group, keyword, sentences, totalCount)
-addGroups(["group1", "group2", "group3"])
-
-setData([
-  {
-    "group": "group1",
-    "keyword": "keyword1",
-    "totalCount": 10,
-    "sentences": ["어려워요group1", "재미있어요", "꺌꺌꺌"],
-  },
-  {
-    "group": "group1",
-    "keyword": "keyword2",
-    "totalCount": 10,
-    "sentences": ["어려워요2group1", "재미있어요2", "꺌꺌꺌2"],
-  },
-  {
-    "group": "group2",
-    "keyword": "keyword1",
-    "totalCount": 20,
-    "sentences": ["어려워요group2", "재미있어요", "꺌꺌꺌"],
-  },
-  {
-    "group": "group3",
-    "keyword": "keyword1",
-    "totalCount": 30,
-    "sentences": ["어려워요group3", "재미있어요", "꺌꺌꺌"],
-  }
-])
+// addGroups(["group1", "group2", "group3"])
+// setData([
+//   {
+//     "group": "group1",
+//     "keyword": "keyword1",
+//     "totalCount": 10,
+//     "sentences": ["어려워요group1", "재미있어요", "꺌꺌꺌"],
+//   },
+//   {
+//     "group": "group1",
+//     "keyword": "keyword2",
+//     "totalCount": 10,
+//     "sentences": ["어려워요2group1", "재미있어요2", "꺌꺌꺌2"],
+//   },
+//   {
+//     "group": "group2",
+//     "keyword": "keyword1",
+//     "totalCount": 20,
+//     "sentences": ["어려워요group2", "재미있어요", "꺌꺌꺌"],
+//   },
+//   {
+//     "group": "group3",
+//     "keyword": "keyword1",
+//     "totalCount": 30,
+//     "sentences": ["어려워요group3", "재미있어요", "꺌꺌꺌"],
+//   }
+// ])
 // 삭제
 // deleteKeyword("임원", "팀플")
 // deleteDocs("Groups")
